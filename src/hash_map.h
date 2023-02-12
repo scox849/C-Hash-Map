e94f7da1d3fe7deac3a20f596a7da01198e9c1f7
Sam Cox <scox849@gmail.com>
2023-02-11 23:16:50 -0700

    Formatting

--- src/hash_map.h
+++ src/hash_map.h
@@ -5,67 +5,65 @@
 
 template <typename K, typename V>
 
-class Map{
+class Map {
+public:
+  Map() {}
 
-	public: 
-		Map(){}
+  ~Map() {}
 
-		~Map(){}
-		
-		/*
-		 Clears all values from the hashmap and resets the size of vectors to zero.
-		 */
-		void clear(){
-			for(int i = 0; i < MAX_MAP_SIZE; i++){
-				map_values[i].clear();
-			}
-		}
-		
-		/*
-		  Gets a value from the hashmap given a key,
-			Index in arrary of vectors is retrieved from hash function on key.
-			Vector at index is iterated over until equal keys are found.
-			Value associated with key is returned.
-		*/ 
-		V get(K key){
-			std::size_t key_hash = std::hash<K>()(key);
-			key_hash %= MAX_MAP_SIZE;
-			int index = static_cast<int>(key_hash);
-			V value = V{};
+  /*
+   Clears all values from the hashmap and resets the size of vectors to zero.
+   */
+  void clear() {
+    for (int i = 0; i < MAX_MAP_SIZE; i++) {
+      map_values[i].clear();
+    }
+  }
 
-			for(map_pair p: map_values[index]){
-				if(p.keyObject == key){
-					value = p.valueObject;
-				}
-			}
+  /*
+    Gets a value from the hashmap given a key,
+          Index in arrary of vectors is retrieved from hash function on key.
+          Vector at index is iterated over until equal keys are found.
+          Value associated with key is returned.
+  */
+  V get(K key) {
+    std::size_t key_hash = std::hash<K>()(key);
+    key_hash %= MAX_MAP_SIZE;
+    int index = static_cast<int>(key_hash);
+    V value = V{};
 
-			return value;
-		}
+    for (map_pair p : map_values[index]) {
+      if (p.keyObject == key) {
+        value = p.valueObject;
+      }
+    }
 
-		/*
-		 Inserts a key value pair into the hashmap.
-		 Index in arrary of vectors is gained from hash funciton.
-		 Pair is appended to end of vector at index.
-		 */
-		void put(K key, V value){
-			std::size_t key_hash = std::hash<K>()(key);
-			key_hash %= MAX_MAP_SIZE;
-			int index = static_cast<int>(key_hash);
-			map_pair new_pair;
-			
-			new_pair.keyObject = key;
-		  new_pair.valueObject = value;
-			map_values[index].push_back(new_pair);
-		}
+    return value;
+  }
 
-	private:
-		struct map_pair{
-			K keyObject;
-			V valueObject;
-		};
-		
-		typedef struct map_pair map_pair;
-		std::vector<map_pair> map_values[MAX_MAP_SIZE]; //array of vectors representing hashmap
+  /*
+   Inserts a key value pair into the hashmap.
+   Index in arrary of vectors is gained from hash funciton.
+   Pair is appended to end of vector at index.
+   */
+  void put(K key, V value) {
+    std::size_t key_hash = std::hash<K>()(key);
+    key_hash %= MAX_MAP_SIZE;
+    int index = static_cast<int>(key_hash);
+    map_pair new_pair;
+
+    new_pair.keyObject = key;
+    new_pair.valueObject = value;
+    map_values[index].push_back(new_pair);
+  }
+
+private:
+  struct map_pair {
+    K keyObject;
+    V valueObject;
+  };
+
+  typedef struct map_pair map_pair;
+  std::vector<map_pair>
+      map_values[MAX_MAP_SIZE]; // array of vectors representing hashmap
 };
-
-
--- src/test_hash.cpp
+++ src/test_hash.cpp
@@ -1,20 +1,15 @@
 #include "hash_map.h"
 #include <string>
 
-
-void add_some(Map<int, char>* the_map){
-  
+void add_some(Map<int, char> *the_map) {
   int new3 = 598;
   char new4 = 'd';
-	the_map->put(new3, new4);
-
+  the_map->put(new3, new4);
 }
 
+int main() {
+  Map<std::string, std::string> the_map;
 
-int main(){
-
-	Map<std::string, std::string> the_map;
-  
   std::string test1 = "key";
   std::string test2 = "c";
   std::string test3 = "value";
@@ -26,45 +21,44 @@ int main(){
   the_map.put(test2, test4);
   the_map.put(test5, test6);
 
-	std::string getval1 = the_map.get(test1);
-	std::string getval2 = the_map.get(test2);
-	std::string getval3 = the_map.get(test5);
+  std::string getval1 = the_map.get(test1);
+  std::string getval2 = the_map.get(test2);
+  std::string getval3 = the_map.get(test5);
 
-	std::cout << "Test 1: " << getval1 << std::endl;
-	std::cout << "Test 2: " << getval2 << std::endl;
-	std::cout << "Test 3: " << getval3 << std::endl;
+  std::cout << "Test 1: " << getval1 << std::endl;
+  std::cout << "Test 2: " << getval2 << std::endl;
+  std::cout << "Test 3: " << getval3 << std::endl;
   the_map.clear();
 
   getval1 = the_map.get(test1);
   getval2 = the_map.get(test2);
   getval3 = the_map.get(test5);
-	std::cout << "Test 1: " << getval1 << std::endl;
-	std::cout << "Test 2: " << getval2 << std::endl;
-	std::cout << "Test 3: " << getval3 << std::endl;
+  std::cout << "Test 1: " << getval1 << std::endl;
+  std::cout << "Test 2: " << getval2 << std::endl;
+  std::cout << "Test 3: " << getval3 << std::endl;
 
-	Map<int, char> new_map;
-	
+  Map<int, char> new_map;
+
   int new1 = 10;
   char new2 = 'c';
-	new_map.put(new1, new2);
+  new_map.put(new1, new2);
   add_some(&new_map);
-	char get1 = new_map.get(new1);
-	char get2 = new_map.get(598);
+  char get1 = new_map.get(new1);
+  char get2 = new_map.get(598);
   char get3 = new_map.get(5);
 
-	std::cout << "Get1: " << get1 << std::endl;
-	std::cout << "Get2: " << get2 << std::endl;
-	std::cout << "Get3: " << get3 << std::endl;
+  std::cout << "Get1: " << get1 << std::endl;
+  std::cout << "Get2: " << get2 << std::endl;
+  std::cout << "Get3: " << get3 << std::endl;
 
   Map<int, int> even_newer;
 
   int fir = 9;
   int sec = 5;
 
-  even_newer.put(fir,sec);
+  even_newer.put(fir, sec);
   int check = even_newer.get(fir);
 
   std::cout << "check " << check << std::endl;
   std::cout << "check2 " << even_newer.get(65) << std::endl;
-
 }
